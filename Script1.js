dd748ce2facc262cbaf3ee7d8151f77420a5aac8
Jeffery Ziel <86206346+jrjziel@users.noreply.github.com>
2022-08-03 08:32:03 -0500

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+let myAge = 33;
+
+const earlyYears = 2 * 10.5;
+
+let laterYears = (myAge - 2) * 4;
+
+const myAgeInDogYears = earlyYears + laterYears;
+
+myName = "Jeffery".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
